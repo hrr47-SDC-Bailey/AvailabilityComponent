66f9fb55d0bb977a96ed8633160e92de40d86aa9
Steven Trager <steventrager1996@gmail.com>
2020-08-10 15:27:46 -0700

    create grid, start populating data from api

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './css/styles.css';
+import axios from 'axios';
+import styles from './css/main.css';
+import CheckAvailability from './checkAvailability.jsx';
+import MySelection from './MySelection.jsx';
 
-const App = () => (
-  <div className={styles.background}>
-    <h1>React is rendering and styling</h1>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/hostel/1/rooms')
+      .then((result) => {
+        this.setState({
+          rooms: result.data,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <CheckAvailability rooms={this.state.rooms} />
+        <MySelection />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/mySelection.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './css/mySelection.css';
+
+function MySelection(props) {
+  return <div className={styles.selectionContainer}>This is The Selection Component</div>;
+}
+
+export default MySelection;
