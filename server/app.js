2877c41502bacb6c867b8a4d2a92bb3ae910cdda
Tyler Jones <tejones1102@gmail.com>
2020-09-03 16:40:53 -0700

    Delete request working.

--- server/app.js
+++ server/app.js
@@ -48,95 +48,15 @@ app.post('/api/hostel/:hostelId', (req, res) => {
   })
 })
 
-// app.get('/api/hostel/:hostelId/rooms', (req, res) => {
-//   const id = req.params.hostelId;
-//   if (!Number.isNaN(Number(id))) {
-//     query.getRoomsByHostel(id)
-//       .then((result) => {
-//         res.json(result);
-//       })
-//       .catch(() => {
-//         res.status(400).send('something went wrong');
-//       });
-//   } else {
-//     res.send('Provide a numeric hostelId');
-//   }
-// });
-
-// app.get('/api/hostel', (req, res) => {
-//   query.getHostels()
-//     .then((result) => {
-//       res.json(result);
-//     })
-//     .catch(() => {
-//       res.status(400).send('something went wrong');
-//     });
-// });
-
-// app.get('/api/hostel/room/reservation', (req, res) => {
-//   res.send('endpoint under maintenance');
-// });
-
-// app.put('/api/hostel', (req, res) => {
-//   query.updateHostel(req.body, (error, updatedHostel) => {
-//     if (error) {
-//       res.status(502).send();
-//     } else {
-//       res.send(updatedHostel);
-//     }
-//   });
-// });
-
-// app.put('/api/hostel/:hostelId/rooms', (req, res) => {
-//   query.updateRoom(req.body, (error, updatedRoom) => {
-//     if (error) {
-//       res.status(502).send();
-//     } else {
-//       res.send(updatedRoom);
-//     }
-//   });
-// });
-
-// app.post('/api/hostel', (req, res) => {
-//   query.createHostel(req.body, (error, newHostel) => {
-//     if (error) {
-//       res.status(502).send();
-//     } else {
-//       res.send(newHostel);
-//     }
-//   });
-// });
-
-// app.post('/api/hostel/:hostelId/rooms', (req, res) => {
-//   query.createRoom(req.body, (error, newRoom) => {
-//     if (error) {
-//       res.status(502).send();
-//     } else {
-//       res.send(newRoom);
-//     }
-//   });
-// });
-
-// app.delete('/api/hostel', (req, res) => {
-//   console.log(req.body.id);
-//   query.deleteHostel(req.body, (error, deletedHostel) => {
-//     if (error) {
-//       res.status(502).send(error);
-//     } else {
-//       res.send(deletedHostel);
-//     }
-//   });
-// });
-
-// app.delete('/api/hostel/:hostelId/rooms', (req, res) => {
-//   query.deleteRoom(req.body, (error, deletedRoom) => {
-//     if (error) {
-//       res.status(502).send(error);
-//     } else {
-//       res.send(deletedRoom);
-//     }
-//   });
-// });
+app.delete('/api/hostel/:hostelId', (req, res) => {
+  query.deleteRoom(req.body.id, (error, deletedRoom) => {
+    if (error) {
+      res.status(502).send();
+    } else {
+      res.send('Room Deleted')
+    }
+  })
+})
 
 
 app.listen(3009);
--- server/database/query.js
+++ server/database/query.js
@@ -7,7 +7,6 @@ const db = new Client({
   host: 'localhost',
   password: null,
   database: 'hostels',
-  // port: 3009
 });
 
 
@@ -62,146 +61,24 @@ const createRoom = (newRoom, callback) => {
   })
 }
 
+const deleteRoom = (id, callback) => {
+  const queryStr = `DELETE FROM rooms WHERE hostel_id = $1`;
+  const queryArgs = [id];
 
-/////////////////////////// CASANDRA QUERIES ///////////////////////////////////
-///////NOT FUNCTIONING YET///////////
-// const cassandra = require ('cassandra-driver');
-// const client = new cassandra.Client({contactPoints: ['Test Cluster', keyspace:'hostels']});
-// client.connect((err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log('CONNECTED CASSANDRA');
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// const mysql = require('mysql');
-// const credentials = require('./config.js');
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: credentials.username,
-//   password: credentials.password,
-//   database: credentials.database,
-//   multipleStatements: true,
-// });
-
-// connection.connect();
-
-// const getHostels = function getHostels() {
-//   return new Promise((resolve, reject) => {
-//     connection.query('SELECT * FROM hostels', (error, results) => {
-//       if (error) {
-//         reject('error');
-//       } else {
-//         resolve(results);
-//       }
-//     });
-//   });
-// };
-
-
-
-// const getRoomsByHostel = function getRoomsByHostel(hostelId) {
-//   return new Promise((resolve, reject) => {
-//     connection.query('SELECT * FROM rooms where hostel_id = ?', [hostelId], (error, results) => {
-//       if (error) {
-//         reject('error');
-//       } else {
-//         resolve(results);
-//       }
-//     });
-//   });
-// };
-
-// const updateHostel = (hostelToUpdate, callback) => {
-//   connection.query('UPDATE `hostels`  SET `name` = ?, `currency` = ? where `id` = ?', [hostelToUpdate.name, hostelToUpdate.currency, hostelToUpdate.id], (error, results) => {
-//     if (error) {
-//       callback('error', null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-// const updateRoom = (roomToUpdate, callback) => {
-//   connection.query('UPDATE `rooms` SET `name` = ?, `description` = ?, `type` = ?, `quantity` = ? where `id` = ?', [roomToUpdate.name, roomToUpdate.description, roomToUpdate.type, roomToUpdate.quantity, roomToUpdate.id], (error, results) => {
-//     if (error) {
-//       callback('error', null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-// const createHostel = (hostel, callback) => {
-//   const name = hostel.name;
-//   const currency = hostel.currency;
-//   connection.query('INSERT INTO `hostels` (name, currency) VALUES (?, ?)', [name, currency], (error, results) => {
-//     if (error) {
-//       callback('error', null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-// const createRoom = (room, callback) => {
-//   const name = room.name;
-//   const description = room.description;
-//   const type = room.type;
-//   const hostelid = room.hostel_id;
-//   const quantity = room.quantity;
-//   connection.query('INSERT INTO `rooms` (name, description, type, hostelid, quantity) VALUES (?, ?, ?, ?, ?)', [name, description, type, hostelid, quantity], (error, results) => {
-//     if (error) {
-//       callback('error', null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-// const deleteHostel = (hostelToDelete, callback) => {
-//   connection.query('DELETE FROM `rooms` WHERE `hostel_id` = ?', [hostelToDelete.id]);
-//   connection.query('DELETE FROM `hostels` WHERE `id` = ?', [hostelToDelete.id], (error, results) => {
-//     if (error) {
-//       callback(error, null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-// const deleteRoom = (roomToDelete, callback) => {
-//   connection.query('DELETE FROM `rooms` WHERE `hostel_id` = ?', [roomToDelete.id], (error, results) => {
-//     if (error) {
-//       callback(error, null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-
+  db.query(queryStr, queryArgs, (error, results) => {
+    if (error) {
+      callback('error', null);
+    } else {
+      callback(null, results);
+    }
+  })
+}
 
 
 module.exports = {
  getRoomById,
  updateRoomById,
  createRoom,
-// getRoomsByHostel
+ deleteRoom
+
 };
