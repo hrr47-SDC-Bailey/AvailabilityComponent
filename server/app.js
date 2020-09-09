695a14829ecea3a3b903ce1fc68b10e4f1ac4468
Tyler Jones <tejones1102@gmail.com>
2020-09-09 16:09:01 -0700

    Updated api routes to use deployed service.

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@
   });
   app.use(cors());
   app.use(express.static(path.join(__dirname, '../public')));
-  app.use('/hostels/:hostelId/rooms', express.static(path.join(__dirname, '../public')));
+  app.use('/hostels/:hostelId', express.static(path.join(__dirname, '../public')));
 
 
 
