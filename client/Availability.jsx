5c1bdb82a9bdeed28446d3c417ada65a28d6c530
Tyler Jones <tejones1102@gmail.com>
2020-09-09 17:11:44 -0700

    Fixed error.

--- client/Availability.jsx
+++ client/Availability.jsx
@@ -3,33 +3,34 @@ import styles from './css/Availability.css';
 import Rooms from './Rooms.jsx';
 
 const Availability = (props) => {
-  if (props.rooms === undefined) {
-    return null;
-  }
-  const privateRooms = props.rooms.filter((room) => room.type === 'private');
-  const dormRooms = props.rooms.filter((room) => room.type === 'dorm');
-  return (
-    <div className={styles.AvailabilityContainer}>
-      <h4 className={styles.checkAvailability}>Check Availability</h4>
-      <div className={styles.privateRooms}>
-        <div className={styles.privateRoomsLeft}>
-          <div className={styles.privateRoom}>Private Rooms</div>
-          <div className={styles.perRoom}>Prices are per room</div>
-        </div>
-        <div className={styles.privateRoomsRight}>
-          <div className={styles.privateRoomRate}>
-            <div className={styles.privateratecontainer}>
-              <span>Average Price per Night</span>
-              <span>Rooms</span>
+  if (props.rooms !== undefined) {
+    const privateRooms = props.rooms.filter((room) => room.type === 'private');
+    const dormRooms = props.rooms.filter((room) => room.type === 'dorm');
+    return (
+      <div className={styles.AvailabilityContainer}>
+        <h4 className={styles.checkAvailability}>Check Availability</h4>
+        <div className={styles.privateRooms}>
+          <div className={styles.privateRoomsLeft}>
+            <div className={styles.privateRoom}>Private Rooms</div>
+            <div className={styles.perRoom}>Prices are per room</div>
+          </div>
+          <div className={styles.privateRoomsRight}>
+            <div className={styles.privateRoomRate}>
+              <div className={styles.privateratecontainer}>
+                <span>Average Price per Night</span>
+                <span>Rooms</span>
+              </div>
             </div>
           </div>
         </div>
+        <div className={styles.roomContainer}>
+          {privateRooms.map((room, i, arr) => <Rooms min={arr[0].id} room={room} handleChoose={props.handleChoose} key={room.id} />)}
+        </div>
       </div>
-      <div className={styles.roomContainer}>
-        {privateRooms.map((room, i, arr) => <Rooms min={arr[0].id} room={room} handleChoose={props.handleChoose} key={room.id} />)}
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Availability;
